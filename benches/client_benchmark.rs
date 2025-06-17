2fa131365dfc6c4ebf056e4b488a3999199473e0
Peter Csajtai <peter.csajtai@outlook.com>
2025-06-17 12:26:13 +0200

    Replace mutex in config service with atomic config entry / semaphore (#9)
    * Replace mutex in config service with atomic config entry / semaphore
    
    * Update service.rs
    
    * Add benchmark for get_value
    
    * Update client_benchmark.rs
    
    * Update client_benchmark.rs
    
    * Update client_benchmark.rs
    
    * Add increasing reader values to benchmark

---
+++ benches/client_benchmark.rs
@@ -0,0 +1,69 @@
+use chrono::{DateTime, Utc};
+use configcat::{Client, ConfigCache, PollingMode};
+use criterion::Criterion;
+use criterion::{criterion_group, criterion_main};
+use std::sync::Arc;
+use tokio::runtime::Runtime;
+
+struct SingleValueCache {
+    value: String,
+}
+
+impl SingleValueCache {
+    pub fn new(val: String) -> Self {
+        Self { value: val }
+    }
+}
+
+impl ConfigCache for SingleValueCache {
+    fn read(&self, _: &str) -> Option<String> {
+        Some(self.value.clone())
+    }
+    fn write(&self, _: &str, _: &str) {}
+}
+
+fn get_value_bench(c: &mut Criterion) {
+    let client = Arc::new(
+        Client::builder("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ")
+            .polling_mode(PollingMode::Manual)
+            // We benchmark on a cache to bypass the first HTTP request which
+            // heavily influences the measurements.
+            .cache(Box::new(SingleValueCache::new(construct_cache_payload(
+                true,
+                Utc::now(),
+                "tag",
+            ))))
+            .build()
+            .unwrap(),
+    );
+    let samples = [100, 1000, 10000, 100000];
+
+    for i in samples.iter() {
+        let sample_size = *i;
+        c.bench_function(format!("get_value {}", sample_size).as_str(), |b| {
+            b.to_async(Runtime::new().unwrap()).iter(|| async {
+                let mut handles = Vec::with_capacity(sample_size);
+                for _ in 0..sample_size {
+                    let cl = client.clone();
+                    handles.push(tokio::spawn(async move {
+                        cl.get_value("testKey", false, None).await
+                    }));
+                }
+                for handle in handles {
+                    handle.await.unwrap();
+                }
+            });
+        });
+    }
+}
+
+fn construct_cache_payload(val: bool, time: DateTime<Utc>, etag: &str) -> String {
+    time.timestamp_millis().to_string() + "\n" + etag + "\n" + &construct_json_payload(val)
+}
+
+fn construct_json_payload(val: bool) -> String {
+    format!(r#"{{"f": {{"testKey":{{"t":0,"v":{{"b": {val}}}}}}}, "s": []}}"#)
+}
+
+criterion_group!(benches, get_value_bench);
+criterion_main!(benches);
